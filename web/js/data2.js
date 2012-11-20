662fed8d091d5c64013fe799f4ca1b325cec3b71
Matthias Schmitt <mschmitt@db12.de>
2012-11-20 11:56:01 +0100

    json direkt

---
+++ web/js/data2.js
@@ -0,0 +1,35 @@
+﻿ var Request = {	
+ 	parameter: function(name) {
+ 		return this.parameters()[name];
+ 	},
+ 	
+ 	parameters: function() {
+ 		var result = {};
+ 		var url = window.location.href;
+ 		var parameters = url.slice(url.indexOf('?') + 1).split('&');
+ 		
+ 		for(var i = 0;  i < parameters.length; i++) {
+ 			var parameter = parameters[i].split('=');
+ 			result[parameter[0]] = parameter[1];
+ 		}
+ 		return result;
+ 	}
+ };
+ 
+
+
+
+function publish(templ, json, target) {
+	var html, json, template;
+    template = $(templ).html();
+    //json = $.parseJSON(jsonstr);
+    html = Mustache.to_html(template, json).replace(/^\s*/mg, '');
+    $(target).html(html).show();
+    $(".pagebreak").css("page-break-before", "always");
+	
+}
+
+$(document).ready( function() {
+	publish('#template', overview, '#content');
+});
+
